2b7ea34839b1e43e99bd86f0403cc193546c9e36
Aizen Codes <arijeetkumarvashishtha@gmail.com>
2024-10-25 10:17:20 +0530

    Add files via upload
    Automated message code

---
+++ Untitled-1.py
@@ -0,0 +1,11 @@
+#code developer and editor:Aizen(arijeet)
+
+import pywhatkit as kit
+
+phone_number = "+919999152297"
+message = "This is me , The great Arijeet"
+hour= 10
+minute = 7
+
+
+kit.sendwhatmsg(phone_number , message , hour , minute)
